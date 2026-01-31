d19d9667cd709c61faf3f9d2d822e9034aa20ce6
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2026-01-31 22:08:54 +0600

    Added HomeComponents folder, Removed the root page design and added 2 new fonts in the layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,8 @@
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-col items-center justify-center gap-8 py-32 px-16">
-        <h1 className="text-4xl font-bold text-black dark:text-white">
-          Welcome to Distak
-        </h1>
-        <button className="px-6 py-3 bg-black text-white dark:bg-white dark:text-black rounded-lg font-medium hover:opacity-90 transition-opacity">
-          Explore
-        </button>
-      </main>
+    <div className="">
+      
     </div>
   );
 }
