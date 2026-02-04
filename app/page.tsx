6bfc544149657921c3739cfc9d4936d414b12207
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2026-02-04 19:17:42 +0600

    Added the Banner section in the root page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Banner from "./HomeComponents/Banner/Banner";
 import FaQ from "./HomeComponents/FaQ/FaQ";
 import GetStarted from "./HomeComponents/GetStarted/GetStarted";
 import SafetyAndSecurity from "./HomeComponents/SafetyAndSecurity/SafetyAndSecurity";
@@ -5,6 +6,7 @@ import SafetyAndSecurity from "./HomeComponents/SafetyAndSecurity/SafetyAndSecur
 export default function Home() {
   return (
     <div className="max-w-275 mx-auto px-4">
+      <Banner />
       <GetStarted />
       <SafetyAndSecurity />
       <FaQ />
