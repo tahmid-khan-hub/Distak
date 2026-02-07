feaf4219c32a4d1e0e6f6ce7fe507b76f7573abd
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2026-02-07 23:29:52 +0600

    Added AnimateOnView hooks, imported it inside the GetStartedContainer

--- app/HomeComponents/GetStarted/components/GetStartedContainer.tsx
+++ app/HomeComponents/GetStarted/components/GetStartedContainer.tsx
@@ -1,3 +1,4 @@
+import AnimateOnView from "@/app/hooks/AnimateOnView";
 import { GetStartedItem } from "../GetStarted";
 import { motion } from "framer-motion";
 
@@ -7,15 +8,17 @@ interface GetStartedContainerProps {
 const GetStartedContainer = ({ items }: GetStartedContainerProps) => {
     return (
         <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
-            {items.map((item) => (
-                <motion.div key={item.id} 
-                    whileHover={{ scale: 1.02, boxShadow: "0px 0px 20px rgba(111, 199, 161, 0.7)", }}
-                    transition={{ duration: 0.2 }} 
-                    className="poppins bg-gray-950 p-6 rounded-lg shadow-lg border-2 border-gray-700 hover:border-[#6FC7A1] transition-colors duration-300">
-                    <div className="text-blue-400 mb-5">{item.icon}</div>
-                    <h3 className="text-xl font-bold text-white mb-2">{item.title}</h3>
-                    <p className="text-gray-300">{item.description}</p>
-                </motion.div>
+            {items.map((item, i) => (
+                <AnimateOnView key={item.id} direction="up" delay={i * 0.08}>
+                    <motion.div key={item.id} 
+                        whileHover={{ scale: 1.02, boxShadow: "0px 0px 20px rgba(111, 199, 161, 0.7)", }}
+                        transition={{ duration: 0.2 }} 
+                        className="poppins bg-gray-950 p-6 rounded-lg shadow-lg border-2 border-gray-700 hover:border-[#6FC7A1] transition-colors duration-300">
+                        <div className="text-blue-400 mb-5">{item.icon}</div>
+                        <h3 className="text-xl font-bold text-white mb-2">{item.title}</h3>
+                        <p className="text-gray-300">{item.description}</p>
+                    </motion.div>
+                </AnimateOnView>
             ))}
         </div>
     );
