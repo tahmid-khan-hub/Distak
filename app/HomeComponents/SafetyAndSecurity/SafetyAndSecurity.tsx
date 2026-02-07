8c64ba1fbba82986964ad357a1fd415301f147cd
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2026-02-07 19:55:42 +0600

    Updated the SafetyAndSecurity icon section

--- app/HomeComponents/SafetyAndSecurity/SafetyAndSecurity.tsx
+++ app/HomeComponents/SafetyAndSecurity/SafetyAndSecurity.tsx
@@ -1,6 +1,7 @@
 "use client"
 import Image from "next/image";
 import SecurityContent from "./components/SecurityContent";
+import { motion } from "framer-motion";
 
 const SafetyAndSecurity = () => {
     return (
@@ -8,13 +9,18 @@ const SafetyAndSecurity = () => {
             <div className="max-w-4xl mx-auto text-center">
                 {/* icon */}
                 <div className="flex justify-center mb-12">
-                    <Image 
-                        src="/assets/shield.png" 
-                        alt="End to End Encryption"
-                        width={200}
-                        height={200}
-                        className="w-48 h-48"
-                    />
+                    <motion.div initial={{ filter: "drop-shadow(0 0 0 rgba(0,0,0,0))" }}
+                    animate={{ filter: "drop-shadow(0 0 0 rgba(0,0,0,0))" }}
+                    whileHover={{ filter: "drop-shadow(0px 0px 20px rgba(111, 199, 161, 0.7))", }}
+                    transition={{ duration: 0.25, ease: "easeOut" }}>
+                        <Image 
+                            src="/assets/shield.png" 
+                            alt="End to End Encryption"
+                            width={200}
+                            height={200}
+                            className="w-48 h-48"
+                        />
+                    </motion.div>
                 </div>
 
                 {/* content */}
