7ac35167d779e9d47071574c4f222c300b158dd0
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2026-03-03 12:23:04 +0600

    Added image for GenerateToken section and updated the structure and design

--- app/HomeComponents/CallToAction/components/GenerateToken/GenerateToken.tsx
+++ app/HomeComponents/CallToAction/components/GenerateToken/GenerateToken.tsx
@@ -1,21 +1,54 @@
 "use client";
 import AnimateOnView from "@/app/hooks/AnimateOnView";
+import Image from "next/image";
 import Link from "next/link";
+import { motion } from "framer-motion";
 
 export default function GenerateToken() {
   return (
-    <div className="py-16 ">
-      <div>
-        {/* Heading */}
+    <section className="relative py-24 px-6 flex items-center justify-center overflow-hidden">
+
+      {/* Background Glow Effects */}
+      <div className="absolute -top-32 -left-32 w-96 h-96 bg-primary/30 blur-3xl rounded-full"></div>
+      <div className="absolute -bottom-32 -right-32 w-96 h-96 bg-primary/30 blur-3xl rounded-full"></div>
+
+      {/* Glass Card */}
+      <div className="">
+
+        {/* Icon */}
         <AnimateOnView direction="up" delay={0.25} duration={0.6}>
-          <h2 className="inter text-4xl text-center font-bold text-white">
+          <div className="flex justify-center mb-12">
+            <motion.div
+              initial={{ filter: "drop-shadow(0 0 0 rgba(0,0,0,0))" }}
+              animate={{ filter: "drop-shadow(0 0 0 rgba(0,0,0,0))" }}
+              whileHover={{
+                filter: "drop-shadow(0px 0px 20px rgba(111, 199, 161, 0.7))",
+              }}
+              transition={{ duration: 0.25, ease: "easeOut" }}
+            >
+              <Image
+                src="/assets/smart-key.png"
+                alt="End to End Encryption"
+                width={200}
+                height={200}
+                className="w-48 h-48"
+              />
+            </motion.div>
+          </div>
+        </AnimateOnView>
+
+        {/* Heading */}
+        <AnimateOnView direction="up" delay={0.3} duration={0.7}>
+          <h2 className="inter text-center text-4xl font-bold 
+            bg-linear-to-r from-primary to-primary/70 
+            bg-clip-text text-transparent">
             Start your first chat
           </h2>
         </AnimateOnView>
 
         {/* Description */}
-        <AnimateOnView direction="up" delay={0.35} duration={0.8}>
-          <p className="poppins mt-2.5 text-center text-gray-400 max-w-xl mx-auto">
+        <AnimateOnView direction="up" delay={0.4} duration={0.8}>
+          <p className="poppins text-center text-gray-400 max-w-lg mx-auto mt-3 text-lg leading-relaxed">
             Generate a unique token and start chatting instantly.
           </p>
         </AnimateOnView>
@@ -31,6 +64,6 @@ export default function GenerateToken() {
           </div>
         </AnimateOnView>
       </div>
-    </div>
+    </section>
   );
 }
