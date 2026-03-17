9094b25ce05e1326f6705cd796ce96fb1f32b96f
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2026-03-17 23:11:01 +0600

    Add Navbar skeleton

--- app/HomeComponents/Navbar/Navbar.tsx
+++ app/HomeComponents/Navbar/Navbar.tsx
@@ -2,20 +2,20 @@
 import { useSession } from "@/app/hooks/useSession"
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import NavbarSkeleton from "./NavbarSkeleton";
 
 export default function Navbar() {
     const { data: session, isLoading } = useSession();
-    if(isLoading) { return <div>Loading...</div> }
     
     return (
         <div className="flex justify-end py-3">
-            {session && 
-                <div>
-                    <Link href={"/conversations"}>
-                    <Button className="">Conversations</Button>
-                    </Link>
-                </div>
-            }
+            {isLoading ? (
+                <NavbarSkeleton />
+            ) : session && (
+                <Link href="/conversations">
+                    <Button>Conversations</Button>
+                </Link>
+            )}
         </div>
     )
 }
---
+++ app/HomeComponents/Navbar/NavbarSkeleton.tsx
@@ -0,0 +1,6 @@
+export default function NavbarSkeleton() {
+    return (
+        <div className="h-9 w-32 rounded-md bg-black animate-pulse">
+        </div>
+    )
+}
