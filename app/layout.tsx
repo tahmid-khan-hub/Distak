f164270e5bee96a3d662207a8b7e2d979b739ba0
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2026-05-13 15:31:40 +0600

    Add new fonts, remove spotlight background

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Inter, Poppins } from "next/font/google";
+import { Inter, Lobster_Two, Poppins } from "next/font/google";
 import "./globals.css";
 import QueryProvider from "./QueryProvider";
 
@@ -14,6 +14,13 @@ const poppins = Poppins({
   weight: ["400", "500", "600", "700"],
 });
 
+const lobsterTwo = Lobster_Two({
+  variable: "--font-lobster-two",
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
+
+
 export const metadata: Metadata = {
   title: "Distak",
 };
@@ -25,18 +32,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.variable} ${poppins.variable} antialiased`}>
+      <body className={`${inter.variable} ${poppins.variable} ${lobsterTwo.variable} antialiased`}>
         <QueryProvider>
           {/* Dark Base Background */}
           <div className="fixed inset-0 -z-20 bg-black" />
-
-          {/* Fixed Spotlight Background */}
-          <div
-            className="fixed inset-0 -z-10 pointer-events-none"
-            style={{
-            background: ` radial-gradient( circle at center, rgba(111, 199, 161, 0.18) 0%, rgba(111, 199, 161, 0.10) 25%, rgba(0, 0, 0, 0) 60% ) `,
-            }}
-          />
 
           {/* Main Content */}
           <div className="relative z-10 ">{children}</div>
